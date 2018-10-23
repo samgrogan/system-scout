01e74a45383fc8abfcf0d4b44c552e3a9971d01b
Sam Grogan <sam@fusionmedia.com>
2018-10-22 21:51:09 -0700

    Start work on cfgmgr wrapper

---
+++ WinScout/WinScoutLib/PInvoke/CfgMgr.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WinScoutLib.PInvoke
+{
+    class CfgMgr
+    {
+        [DllImport("cfgmgr32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern void CM_Enumerate_Classes(
+            [In] ulong         ulClassIndex,
+        [Out]  Guid        ClassGuid,
+        [In] ulong         ulFlags
+        );
+    }
+}
