6c9bc816597eeed2197e0433c71a7c4915f5a6b6
Sam Grogan <sam@fusionmedia.com>
2018-10-24 21:57:57 -0700

    Configure c++ library for testing.

--- WinScout/WinScoutLibTest/UnitTest1.cs
+++ WinScout/WinScoutLibTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinScoutUnmanagedWrapperLib;
 
 namespace WinScoutLibTest
 {
@@ -9,6 +10,7 @@ namespace WinScoutLibTest
         [TestMethod]
         public void TestMethod1()
         {
+            TestClass test = new TestClass();
         }
     }
 }
