246f12e816e974e9f96add95f5d84a8a2a5ee992
Sam Grogan <sam@fusionmedia.com>
2018-11-18 08:49:44 -0800

    Work on detection of installed applications. (Broken)

--- WinScout/CppClrPlayground/CppClrPlayground.cpp
+++ WinScout/CppClrPlayground/CppClrPlayground.cpp
@@ -1,18 +1,20 @@
 #include "stdafx.h"
-#include "Windows.h";
-#include "Msi.h";
+#include "Windows.h"
+#include "Msi.h"
 
 using namespace System;
 
+public ref class 
+
 int main(array<System::String ^> ^args)
 {
-	if (args->Length == 0)
-	{
-		Console::WriteLine("No arguments given.");
-		return -1;
-	}
+	//if (args->Length == 0)
+	//{
+	//	Console::WriteLine("No arguments given.");
+	//	return -1;
+	//}
 
-	if (args[0] == "software")
+	//if (args[0] == "software")
 	{
 		UINT ret;
 		DWORD dwIndex = 0;
@@ -41,24 +43,54 @@ int main(array<System::String ^> ^args)
 
 			if (ret == ERROR_SUCCESS)
 			{
-				char* name = MsiGetProductInfoEx(
-					szInstalledProductCode,
-					cchSid == 0 ? NULL : szSid,
-					dwInstalledContext,
-					INSTALLPROPERTY_INSTALLEDPRODUCTNAME
-				);
+				Console::WriteLine("Enumerated Product " + dwIndex);
+				Console::Write(L"\t");
+				Console::WriteLine(szInstalledProductCode);
+				Console::Write(L"\t");
+				Console::WriteLine(szSid);
 
-				char* version = MsiGetProductInfoEx(
-					szInstalledProductCode,
-					cchSid == 0 ? NULL : szSid,
-					dwInstalledContext,
-					INSTALLPROPERTY_VERSIONSTRING
-				);
+				//wchar_t* name = MsiGetProductInfoEx(
+				//	szInstalledProductCode,
+				//	cchSid == 0 ? NULL : szSid,
+				//	dwInstalledContext,
+				//	INSTALLPROPERTY_INSTALLEDPRODUCTNAME
+				//);
 
-				cout << name << endl;
-				cout << "  - " << version << endl;
+				//wchar_t* version = MsiGetProductInfoEx(
+				//	szInstalledProductCode,
+				//	cchSid == 0 ? NULL : szSid,
+				//	dwInstalledContext,
+				//	INSTALLPROPERTY_VERSIONSTRING
+				//);
 
 				dwIndex++;
+			} else {
+				switch (ret) {
+					case ERROR_ACCESS_DENIED:
+						Console::WriteLine("MsiEnumProductsEx returned ERROR_ACCESS_DENIED");
+						break;
+					case ERROR_BAD_CONFIGURATION:
+						Console::WriteLine("MsiEnumProductsEx returned ERROR_BAD_CONFIGURATION");
+						break;
+					case ERROR_INVALID_PARAMETER:
+						Console::WriteLine("MsiEnumProductsEx returned ERROR_INVALID_PARAMETER");
+						break;
+					case ERROR_NO_MORE_ITEMS:
+						Console::WriteLine("MsiEnumProductsEx returned ERROR_NO_MORE_ITEMS");
+						break;
+					case ERROR_MORE_DATA:
+						Console::WriteLine("MsiEnumProductsEx returned ERROR_MORE_DATA");
+						break;
+					case ERROR_UNKNOWN_PRODUCT:
+						Console::WriteLine("MsiEnumProductsEx returned ERROR_UNKNOWN_PRODUCT");
+						break;
+					case ERROR_FUNCTION_FAILED:
+						Console::WriteLine("MsiEnumProductsEx returned ERROR_FUNCTION_FAILED");
+						break;
+					default:
+						Console::WriteLine("Unknown return value from MsiEnumProductsEx");
+						break;
+				}
 			}
 
 		} while (ret == ERROR_SUCCESS);
