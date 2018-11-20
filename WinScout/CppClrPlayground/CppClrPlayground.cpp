5d917c19d0020504f1412546cf042f95ead6be2c
Sam Grogan <sam@fusionmedia.com>
2018-11-20 13:36:26 -0800

    Added code to retreive installed product name

--- WinScout/CppClrPlayground/CppClrPlayground.cpp
+++ WinScout/CppClrPlayground/CppClrPlayground.cpp
@@ -9,7 +9,65 @@ using namespace System::Collections::Generic;
 ref class InstalledProduct {
 public:
 	property System::String ^ProductCode;
+	property System::String ^ProductName;
 };
+
+// Returns the given property as an unmanaged string
+System::String ^GetInstalledProductProperty(wchar_t* szProductCode, wchar_t* szUserSid, MSIINSTALLCONTEXT dwInstalledContext, wchar_t* szProperty)
+{
+	DWORD value_length = 1024;
+	wchar_t value[1024] = { 0 };
+
+	UINT ret = MsiGetProductInfoEx(
+		szProductCode,
+		szUserSid,
+		dwInstalledContext,
+		szProperty,
+		value,
+		&value_length
+	);
+
+	if (ret == ERROR_SUCCESS)
+	{
+		return gcnew System::String(value);
+	}
+	else
+	{
+		switch (ret)
+		{
+		case ERROR_ACCESS_DENIED:
+			Console::WriteLine("MsiGetProductInfoEx returned ERROR_ACCESS_DENIED");
+			break;
+		case ERROR_BAD_CONFIGURATION:
+			Console::WriteLine("MsiGetProductInfoEx returned ERROR_BAD_CONFIGURATION");
+			break;
+		case ERROR_INVALID_PARAMETER:
+			Console::WriteLine("MsiGetProductInfoEx returned ERROR_INVALID_PARAMETER");
+			break;
+		case ERROR_MORE_DATA:
+			Console::WriteLine("MsiGetProductInfoEx returned ERROR_MORE_DATA");
+			break;
+		case ERROR_SUCCESS:
+			Console::WriteLine("MsiGetProductInfoEx returned ERROR_SUCCESS");
+			break;
+		case ERROR_UNKNOWN_PRODUCT:
+			Console::WriteLine("MsiGetProductInfoEx returned ERROR_UNKNOWN_PRODUCT");
+			break;
+		case ERROR_UNKNOWN_PROPERTY:
+			Console::WriteLine("MsiGetProductInfoEx returned ERROR_UNKNOWN_PROPERTY");
+			break;
+		case ERROR_FUNCTION_FAILED:
+			Console::WriteLine("MsiGetProductInfoEx returned ERROR_FUNCTION_FAILED");
+			break;
+		default:
+			Console::WriteLine("Unknown return value from MsiGetProductInfoEx");
+			break;
+		}
+	}
+
+	return nullptr;
+}
+
 
 int main(array<System::String ^> ^args)
 {
@@ -54,6 +112,7 @@ int main(array<System::String ^> ^args)
 
 				InstalledProduct ^installedProduct = gcnew InstalledProduct();
 				installedProduct->ProductCode = gcnew System::String(szInstalledProductCode);
+				installedProduct->ProductName = GetInstalledProductProperty(szInstalledProductCode, (cchSid == 0 ? NULL : szSid), dwInstalledContext, INSTALLPROPERTY_INSTALLEDPRODUCTNAME);
 
 				//wchar_t* name = MsiGetProductInfoEx(
 				//	szInstalledProductCode,
@@ -70,6 +129,8 @@ int main(array<System::String ^> ^args)
 				//);
 
 				Console::WriteLine(installedProduct->ProductCode);
+				Console::WriteLine(installedProduct->ProductName);
+				Console::WriteLine(L"");
 
 				dwIndex++;
 			} else {
@@ -107,3 +168,4 @@ int main(array<System::String ^> ^args)
 
 	return 0;
 }
+
