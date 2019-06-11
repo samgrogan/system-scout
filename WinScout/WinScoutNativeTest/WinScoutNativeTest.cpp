51b9b9d8c0620908a948ae08e7f5fb34e515e2e5
Sam Grogan <sam@fusionmedia.com>
2019-06-10 18:21:26 -0700

    Create unit test project

---
+++ WinScout/WinScoutNativeTest/WinScoutNativeTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace WinScoutNativeTest
+{
+	TEST_CLASS(WinScoutNativeTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
