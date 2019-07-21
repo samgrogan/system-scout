6e91c20b9111144bc23d5350c319b926e2199945
Sam Grogan <sam@fusionmedia.com>
2019-07-21 13:20:19 -0700

    Work on managed code in interop

--- WinScout/WinScoutInterop/Managed/Device.h
+++ WinScout/WinScoutInterop/Managed/Device.h
@@ -1,11 +1,25 @@
 #pragma once
 
+
 #include "../Unmanaged/UM_Device.h"
+
+using namespace System;
 
 namespace WinScout::Interop::Managed {
 
 	public ref class Device
 	{
+	private:
+		String^ _DeviceId;
+
+	public:
+		// Create a managed device class from an unmanaged device class
+		Device(std::shared_ptr<WinScout::Interop::Unmanaged::Device> UMDevice);
+
+		property String^ DeviceId {
+			String^ get();
+		}
+
 	};
 
 }
