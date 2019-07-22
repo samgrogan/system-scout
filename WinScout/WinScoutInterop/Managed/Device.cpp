da03cac03fed205b0aecbea361d34a3a4445c1ae
Sam Grogan <sam@fusionmedia.com>
2019-07-22 11:29:26 -0700

    Work on managed bridge

--- WinScout/WinScoutInterop/Managed/Device.cpp
+++ WinScout/WinScoutInterop/Managed/Device.cpp
@@ -4,8 +4,16 @@ using namespace WinScout::Interop::Managed;
 
 // Create a managed device class from an unmanaged device class
 Device::Device(std::shared_ptr<WinScout::Interop::Unmanaged::Device> UMDevice) {
+	if (UMDevice == nullptr)
+	{
+		throw ("Can't create a managed Device from a nullptr");
+	}
 
+	// Get the list of properties from the umdevice
+	std::unordered_map<DEVPROPKEY, std::shared_ptr<Unmanaged::DeviceInstanceProperty>> device_properties = UMDevice->EnumerateProperties();
 
+	// Find the standard properties
+	_deviceId = gcnew String(UMDevice->GetDeviceID().c_str());
 }
 
 String^ Device::DeviceId::get() {
